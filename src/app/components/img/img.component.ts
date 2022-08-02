c70c3256e5852f2366592ac8f54975cf8c52dc6b
Jorge Vilca <jlvilcav@gmail.com>
2022-08-01 19:37:20 -0500

    Usando outputs

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,10 +6,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./img.component.scss']
 })
 export class ImgComponent implements OnInit {
-  @Input() img: string = 'Valor inicial';
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imgDefault = './assets/images/default.png';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  imgError(){
+    this.img = this.imgDefault;
+  }
+
+  imgLoad(){
+    console.log('log hijo');
+    this.loaded.emit(this.img);
   }
 
 }
