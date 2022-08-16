698b03694259d9d48e9eb0c51783a8f4e1a34fe7
Jorge Vilca <jlvilcav@gmail.com>
2022-08-16 12:12:36 -0500

    Usando retry en caso ocurra errores de carga

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Product, CreateProductDTO, UpdateProductDTO } from '../models/product.model';
+import { retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  // private apiUrl: string = 'https://young-sands-07814.herokuppapp.com/api/products';
   private apiUrl: string = 'https://young-sands-07814.herokuapp.com/api/products';
 
   constructor(
@@ -20,7 +22,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string) {
@@ -30,7 +35,9 @@ export class ProductsService {
   getProductsByPage(limit: number, offset: number){
     return this.http.get<Product[]>(`${this.apiUrl}`,{
       params: {limit, offset}
-    });
+    }).pipe(
+      retry(3)
+    );
   }
 
   create(dto: CreateProductDTO){
