b9ab89790bd924dc14e7b973a32096520263a80d
Jorge Vilca <jlvilcav@gmail.com>
2022-08-22 00:49:59 -0500

    Evitando doble subscribe

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 import { ProductsService } from './../../services/products.service';
 import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-category',
+  // template: `<app-products [products]="products" (loadMore)="onLoadMore()"></app-products>`,
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
@@ -22,17 +24,29 @@ export class CategoryComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.categoryId = params['id'];
-      if (this.categoryId) {
-        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-          .subscribe(data => {
-            this.products = data;
-            this.offset += this.limit;
-          });
+    this.route.params
+    .pipe(switchMap(params =>{
+        this.categoryId = params['id'];
+        if (this.categoryId) {
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset);
+        }
+        return [];
       }
-
+    ))
+    .subscribe(data => {
+      this.products = data;
     });
+  }
+
+  onLoadMore() {
+    if (this.categoryId) {
+      this.productsService
+        .getByCategory(this.categoryId, this.limit, this.offset)
+        .subscribe((data) => {
+          this.products = this.products.concat(data);
+          this.offset += this.limit;
+        });
+    }
   }
 
 }
