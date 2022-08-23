5667cc49c346fd17a086376d2a71077ad850178f
Jorge Vilca <jlvilcav@gmail.com>
2022-08-23 01:32:19 -0500

    Agregando Parametros Url para category page

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -17,6 +17,7 @@ export class CategoryComponent implements OnInit {
   limit = 10;
   offset = 0;
   products: Product[] = [];
+  productId: string | null = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -35,6 +36,9 @@ export class CategoryComponent implements OnInit {
     ))
     .subscribe(data => {
       this.products = data;
+    });
+    this.route.queryParamMap.subscribe(params => {
+      this.productId = params.get('product');
     });
   }
 
