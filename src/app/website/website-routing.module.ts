605110123a49ec4fc3fa86f60d97c9ffc865d40a
Jorge Vilca <jlvilcav@gmail.com>
2022-08-25 01:44:23 -0500

    Lazy loading y code spliting

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
+
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -18,7 +18,7 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'category/:id', component: CategoryComponent },
+      { path: 'category', loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule) },
       { path: 'product/:id', component: ProductDetailComponent },
       { path: 'mycart', component: MycartComponent },
       { path: 'login', component: LoginComponent },
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -8,7 +8,6 @@ import { SharedModule } from './../shared/shared.module';
 import { NavComponent } from './components/nav/nav.component';
 
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -23,7 +22,6 @@ import { SwiperModule } from 'swiper/angular';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     MycartComponent,
     LoginComponent,
     RegisterComponent,
